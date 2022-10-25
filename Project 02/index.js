d4ca9779bbfddcfd0b21a2a6777390e7bf9dc0d7
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-10-25 19:32:44 +0500

    JS Project 02 TODO APP , update and clear+Complete

--- Project 02/index.js
+++ Project 02/index.js
@@ -1,11 +1,11 @@
 add = document.getElementById("add");
 
 //Event Listener function 
-add.addEventListener("click", update);
+add.addEventListener("click", getAndUpdate);
 update();
 
-//Update fuction
-function update(){
+//Get and Update function
+function getAndUpdate(){
     console.log("updating List.....");
     tit = document.getElementById('title').value;
     desc = document.getElementById('description').value;
@@ -22,8 +22,25 @@ function update(){
         itemJsonArray.push([tit, desc]);
         localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
     }
+    update();
+}
+
+//Update fuction
+function update(){
+
+    if (localStorage.getItem('itemsJson') == null) {
+        itemJsonArray = [];
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+     }
+    else{
+        itemJsonArraystr = localStorage.getItem('itemsJson');
+        itemJsonArray = JSON.parse(itemJsonArraystr);
+        
+    }
+
 
     //when data is received and updated we need to populate the table with data
+    //Populate the table with data
     let tableBody  = document.getElementById('tableBody');
     let str = "";
 
@@ -33,8 +50,26 @@ function update(){
         <th scope="row">${index + 1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
-        <td><button class="btn btn-sm  btn-primary">Delete</button></td>
+        <td><button class="btn btn-sm  btn-primary" onclick="deleted(${index})">Delete</button></td>
       </tr>`       
     });
     document.getElementById('tableBody').innerHTML = str;
+}
+function deleted(itemIndex){
+    console.log("Delete", itemIndex);
+    itemJsonArraystr = localStorage.getItem('itemsJson');
+    itemJsonArray = JSON.parse(itemJsonArraystr);
+    //Delete ItemIndex element fro the array
+    itemJsonArray.splice(itemIndex,1)
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArray));
+    update();  
+}
+function clearStr(){
+    if (confirm("Do you want to really remove all the lists from the storage..")){
+
+    console.log("Clearning the storage.");
+    localStorage.clear();
+    update();
+
+}
 }

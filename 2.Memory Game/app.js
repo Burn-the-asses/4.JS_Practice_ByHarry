b1217115f9e3b0ee92b8673e652e333ce4cbf39c
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-11-04 19:19:09 +0500

    JS Learn by build game , card game almost complete

--- 2.Memory Game/app.js
+++ 2.Memory Game/app.js
@@ -1,3 +1,4 @@
+//Array of card that containing objects
 const cardArray = [
     {
         name: 'fries',
@@ -49,4 +50,52 @@ const cardArray = [
     }
 ]
 
-console.log(cardArray);
+// console.log(cardArray);
+// Shuffle randomoly array elements
+cardArray.sort( () => 0.5 - Math.random());
+// console.log(cardArray);
+
+//Get Display Element Grid
+const gridDisplay = document.querySelector('#grid');
+// console.log(gridDisplay)
+
+const cardsChosen = [];
+const cardsChosenIds = [];
+
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+        const card = document.createElement('img');
+        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('data-id', i);
+        card.addEventListener('click', flipCard);
+        // console.log(card, i);
+        gridDisplay.appendChild(card);
+    }
+}
+createBoard();
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img');
+    console.log('check for match');
+    if(cardsChosen[0] == cardsChosen[1]) {
+        alert('You found a match');
+        cards[cardsChosenIds[0]].setAttribute('src',images/white.png);
+        cards[cardsChosenIds[1]].setAttribute('src',images/white.png);
+        cards[cardsChosenIds[0]].removeEventListener('click', flipCard);
+        cards[cardsChosenIds[1]].removeEventListener('click', flipCard);
+     }
+}
+
+function flipCard() {
+    // console.log(cardArray);
+    const cardId = this.getAttribute('data-id');
+    // console.log(cardArray[cardId].name);
+    cardsChosen.push(cardArray[cardId].name);
+    cardsChosenIds.push(cardId);
+    // console.log('clicked', cardId);
+    // console.log(cardsChosen);
+    this.setAttribute('src', cardArray[cardId].img);
+    if(cardsChosen.length === 2) {
+        setTimeout( checkMatch, 500)
+    }
+}
